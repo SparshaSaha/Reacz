38a737a795664897574cfeeccebb2a859bf3e008
Sparsha Saha <saha.sparsha@gmail.com>
2017-06-06 21:45:06 +0530

    The anroid directory has been added with an initial login activity

---
+++ Source Code/Reacz/app/src/main/java/com/fourthstatelab/reacz/Login_Page.java
@@ -0,0 +1,13 @@
+package com.fourthstatelab.reacz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__page);
+    }
+}
